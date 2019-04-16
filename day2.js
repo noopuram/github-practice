16936f6effc50251309b9319f75da2184fd694ee
learn <learn@learn12.localdomain>
2019-04-16 16:09:13 -0700

    added a file

---
+++ day2.js
@@ -0,0 +1,47 @@
+// var groceryList2 = ['chips', 'dip', 'cookies']
+// groceryList2.push('soda')
+// console.log(groceryList2)
+// var numbers2 = [2,4,6,8,10]
+// numbers2.unshift(0)
+// console.log(numbers2);
+// var groceryList1 = ['apples', 'carrots', 'oatmeal']
+// var g2 = ['granola']
+// console.log(groceryList1.concat(g2))
+
+
+// var groceryList1 = ['apples', 'carrots', 'oatmeal']
+// var g2 = groceryList1.join(["granola"])
+// console.log(g2);
+
+// **********
+
+// ********
+
+// var numbers1 = [1, 2, 3, 4, 5]
+// var num = [0]
+// console.log(num.concat(numbers1));
+// var numbers3 = [2, 13, 6, 8, 4, 2]
+// console.log(numbers3.indexOf(2));
+// console.log(numbers3.lastIndexOf(2));
+//
+// var chars = ["y", "a", "r", "r", "a"]
+// console.log(chars.join(''));
+// var charsReversed = chars.reverse('')
+// console.log(charsReversed.join(''))
+// console.log(charsReversed.join('-'))
+//
+// var parents =['Hal', 'Ellen']
+// var siblings =['Matt', 'Sini']
+// var order = (parents.concat(siblings)).sort()
+// console.log(order.reverse());
+//
+// var answers = ['what would your mother do?', 'Ask: what would an Australian do, then do the opposite', 'Your answer here'.]
+// var question = "How are you?"
+// console.log(answers.Math.random());
+
+
+
+
+
+var answers = ['what would your mother do?', 'Ask: what would an Australian do, then do the opposite', 'Your answer here', 'outlook looks good']
+console.log(answers[Math.floor(Math.random()* answers.length)])
---
+++ day2c.js
@@ -0,0 +1,65 @@
+// var price = 70;
+// if (price<=70) {
+//     console.log("In budget.");
+// }
+// var hungry = "false";
+// if (hungry==true) {
+//     console.log("eat Food.")
+// } else {
+//     console.log("Keep coding.");
+// }
+// var trafficLight = "red";
+// if (trafficLight="green") {
+//     console.log("Go");
+// } else if (trafficLight="red") {
+//     console.log("Stop");
+// } else {
+//     console.log(trafficLight="Slow down");
+// var num1 = 3
+// var num2 = 8
+// if (num1 > num2) {
+//     console.log(num1)
+// } else if
+//     (num1 < num2) {
+//         console.log(num2)
+//     } else {
+//         console.log('Both numbers are equal')
+//     }
+// var num = "Hello"
+// if (num === 0) {
+//     console.log("Zero")
+// } else if
+// (num % 2===0) {
+//     console.log("Even Number")
+// }else if
+//     (num % 2 != 0) {
+//         console.log("Odd number")
+//     } else {
+//         console.log("Invald Entry");
+//     }
+// var grade = 100
+// if (grade == 100) {
+//     console.log("Perfect Score");
+// } else if (grade <=99 && grade >= 90){
+//     console.log("Your grade is A");
+// } else if (grade <=89 && grade >= 80) {
+//     console.log("Your grade is B");
+// } else if (grade <=79 && grade >= 70) {
+//     console.log("Your grade is C");
+// } else if (grade >0 && grade <= 69) {
+//     console.log("failed");
+// } else {
+//     console.log("No grade available");
+// }
+var dataType = ''
+// if (typeof dataType  === "number"){
+//     console.log("This data type is a number")
+// } else if (typeof dataType === "string") {
+//     console.log("This data type is a string");
+// } else if (typeof dataType === "boolean") {
+//     console.log("This data type is boolean");
+// } else {
+//     console.log("This data type is a symbol, undefined, or null");
+// }
+
+console.log(typeof dataType);
