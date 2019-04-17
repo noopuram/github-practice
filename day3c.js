1fe9c01799783f64a8b419ecdaf4c201e808ccbf
noopuram <noopuram_in@yahoo.com>
2019-04-17 11:03:21 -0700

    Add files via upload

---
+++ day3c.js
@@ -0,0 +1,55 @@
+// for (let i=1; i<=20; i++) {
+//     console.log(i);
+// }
+
+// for ( i=1; i <= 20; i++) {
+//     console.log(i*3);
+// }
+// var num = [8,2,17,4,5,10,4,8,9,15]
+// for(let i=0; i<num.length; i++) {
+//     console.log(num[i]*3);
+// }
+//
+// for (let i = 1; i<= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     } else {
+//         console.log("odd");
+//     }
+// }
+
+// for (let i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+// var lett = ["l", "e","a","r","n"]
+// for (let i=0; i<lett.length; i++){
+//     console.log(lett[i].toUpperCase());
+// }
+// var strings = ['hi','yo','there','walk','how','to','where','you']
+// for (let i = 0; i<=strings.length-1; i++){
+//     if (i%2===0){
+//         console.log(strings[i]);
+//     }
+// }
+// for (let i=1; i<=100; i++) {
+//     if (i%3===0 && i%5===0) {
+//         console.log("FizzBuzz");
+//     } else if(i%3===0) {
+//         console.log("Fizz");
+//     }else if (i%5===0){
+//         console.log("Buzz");
+//     }else {
+//         console.log(i);
+//     }
+// }
+
+
+
+
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 === 0) {
+        console.log(i + ' is even')
+    } else {
+        console.log(i + ' is odd')
+    }
+}
